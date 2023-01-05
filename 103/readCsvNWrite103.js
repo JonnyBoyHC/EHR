5806a14d580fa3384917252b99d52ad4095cc565
JonnyBoy <jonnyboyhc@gmail.com>
2023-01-05 22:09:06 +0800

    Refining transefering time with home network

--- 103/readCsvNWrite103.js
+++ 103/readCsvNWrite103.js
@@ -1,6 +1,6 @@
 'use strict';
 import fs from 'fs';
-const csv = fs.readFileSync('./adm103.csv');
+const csv = fs.readFileSync('./adm103_1.csv');
 const array = csv.toString().split('\r\n').join().split(',');
 
 import xlsx from 'xlsx';
