1e8909e8e39616e462ec9db82267010b214ffba7
hcchong <hcchong@msy.edu.mo>
2023-01-06 14:54:00 +0800

    Working on Writting Class

--- 102/readNWrite5-Chains-UpdateEachRequestFirstApproach-AddingClass.js
+++ 102/readNWrite5-Chains-UpdateEachRequestFirstApproach-AddingClass.js
@@ -64,37 +64,119 @@ class Web3Connection {
     return await this.contractAccess.methods.getAllRecords(_subjectID).call({ from: this.coinbaseAddr });
   }
 
+  readingTime = () => {
+    return this.startReadingTime = new Date().getTime();
+  }
+
 }
+
+class Comparison {
+  constructor(list1, list2) {
+    this.list1 = list1;
+    this.list2 = list2;
+  }
+
+  compare() {
+    let results = [];
+    let forDeletion = [];
+    for (let i = 0; i < this.list1.length; i++) {
+      for (let j = 0; j < this.list2.length; j++) {
+        if (JSON.stringify(this.list1[i]) === JSON.stringify(this.list2[j])) {
+          forDeletion.push(this.list1[i]);
+        }
+      }
+    }
+    return this.results = this.list1.filter((item) => !forDeletion.includes(item));
+  }
+
+  comparingTime = () => {
+    return this.startComparingTime = new Date().getTime()
+  }
+
+  writingTime = () => {
+    return this.startWritingTime = new Date().getTime()
+  }
+}
+
+// class DataWriter {
+//   constructor(contract, data) {
+//     this.contract = contract;
+//     this.data = data;
+//   }
+
+//   async write(patientId) {
+//     let startWritingTime = new Date().getTime();
+//     console.log(`Start Writing TimeStamp: ${startWritingTime}\n`);
+
+//     if (this.data.length === 0) {
+//       console.log(`Nothing to write for Patient ID ${patientId}...!!!`);
+//     } else {
+//       console.log('Writing... Patient ID:', patientId);
+//     }
+
+//     // Writing Records
+//     for (let i = 0; i < this.data.length; i++) {
+//       console.log(this.data[i].HadmID);
+//       this.contract.methods
+//         .addNewPatient(patientId, [
+//           +this.data[i].HadmID,
+//           +this.data[i].AdmitTime,
+//           +this.data[i].DischTime,
+//           +this.data[i].DeathTime,
+//           this.data[i].Admission_Type,
+//           this.data[i].Admission_Location,
+//           this.data[i].Discharge_Location,
+//           this.data[i].Insurance,
+//         ])
+//         .send({ from: coinbaseAddr102 });
+//       await sleep(50);
+//     }
+//   }
+// }
+
+
+const beginTimeStamp = new Date().getTime();
+console.log(`Beginning TimeStamp: ${beginTimeStamp}\n`);
+
+const network102 = new Web3Connection(node102, abi102, contractAddr102, coinbaseAddr102);
+const network103 = new Web3Connection(node103, abi103, contractAddr103, coinbaseAddr103);
+const network104 = new Web3Connection(node104, abi104, contractAddr104, coinbaseAddr104);
+const network105 = new Web3Connection(node105, abi105, contractAddr105, coinbaseAddr105);
+const network106 = new Web3Connection(node106, abi106, contractAddr106, coinbaseAddr106);
 
 const init = async () => {
 
-  const beginTimeStamp = new Date().getTime();
-  console.log(`Beginning TimeStamp: ${beginTimeStamp}\n`);
 
-  const network102 = new Web3Connection(node102, abi102, contractAddr102, coinbaseAddr102);
-  const network103 = new Web3Connection(node103, abi103, contractAddr103, coinbaseAddr103);
-  const network104 = new Web3Connection(node104, abi104, contractAddr104, coinbaseAddr104);
-  const network105 = new Web3Connection(node105, abi105, contractAddr105, coinbaseAddr105);
-  const network106 = new Web3Connection(node106, abi106, contractAddr106, coinbaseAddr106);
 
   for (let i = +startNo; i < +stopNo + 1; i++) {
     console.log('####################################################################################');
     console.log('####################################################################################');
     console.log('Retrieving Patient ID: ', i);
 
-    console.log('<<==========================<<   103   >>==========================>>');
-    const startReadingTime103 = new Date().getTime();
-    console.log(`Start Reading 103 TimeStamp: ${startReadingTime103}\n`);
+    console.log(`<<==========================<<   103 --> Patient ID: ', ${i}  >>==========================>>`);
+    console.log(`Start Reading 102 TimeStamp: ${network102.readingTime()}`);
+    const result102 = await network102.accessNetwork(i);
 
-    const result102 = await network102.accessNetwork(args[i]);
-    console.log('Total Records on 102: ', result102);
+    console.log(`Start Reading 103 TimeStamp: ${network103.readingTime()}\n`);
+    const result103 = await network103.accessNetwork(i);
 
+    console.log('<<=====================<<   Comparing...   >>=====================>>');
+    const comparison32 = new Comparison(result103, result102);
+    console.log(`Start Comparing TimeStamp: ${comparison32.comparingTime()}\n`);
+    comparison32.compare()
+    console.log(comparison32.results);
 
-    const result103 = [await network103.accessNetwork(args[i])];
-    console.log('Total Records on 103: ', result103, '\n');
+    console.log('<<=======================<<   Writing...   >>=======================>>');
+    // console.log(`Start Writing TimeStamp: ${comparison32.startWritingTime()}\n`);
+
+    // const contract = new web3.eth.Contract(...);
+    // const data = [{ id: 1, value: 10 }, { id: 2, value: 20 }, { id: 3, value: 30 }];
+    // const writer = new DataWriter(contract, data);
+    // writer.write(123);
 
 
   }
+
 
   console.log('Done!!!\n');
 
